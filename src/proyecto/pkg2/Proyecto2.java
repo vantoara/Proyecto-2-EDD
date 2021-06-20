f178203c9307ac4c5b5af795728662d3d41f19e3
Liliana Nóbrega <lilisabrina2009@gmail.com>
2021-06-19 20:33:43 -0400

    jfilechooser agregado con ventana inicial

--- src/proyecto/pkg2/Proyecto2.java
+++ src/proyecto/pkg2/Proyecto2.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package proyecto.pkg2;
-
+import Interface.beginningWindow;
 /**
  *
  * @author vadau
@@ -15,20 +15,22 @@ public class Proyecto2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        BinaryHeap test = new BinaryHeap();
-        test.insert(1);
-        test.insert(5);
-        test.insert(3);
-        test.insert(50);
-        test.insert(10);
-        test.insert(20);
-        test.insert(2);
+//        BinaryHeap test = new BinaryHeap();
+//        test.insert(1);
+//        test.insert(5);
+//        test.insert(3);
+//        test.insert(50);
+//        test.insert(10);
+//        test.insert(20);
+//        test.insert(2);
+//
+//        test.preorder(test.getMin());
+//        
+//        System.out.println("test");
+//        TestNode node = test.getMin();
+//        System.out.println(node.getRightChild().getRightChild().getData());
+        Interface.beginningWindow w = new beginningWindow();
 
-        test.preorder(test.getMin());
-        
-        System.out.println("test");
-        TestNode node = test.getMin();
-        System.out.println(node.getRightChild().getRightChild().getData());
     }
     
 }
---
+++ src/proyecto/pkg2/csvFile.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.pkg2;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import javax.swing.JOptionPane;
+
+/**
+ * Clase relacionada a la lectura y escritura en archivo csv
+ * @author Liliana Nóbrega
+ */
+public class csvFile {
+    
+    /**
+     * Función para leer un archivo csv y obtener de sus datos una lista de usuarios registrados
+     * @param path string con la dirección del archivo csv del que se leerán los datos
+     * @return la lista de usuarios registrados obtenida de los datos del csv o null si hay un error en la lectura del archivo
+     */
+    public static UserList readCSV(String path){
+        UserList users = new UserList();
+        String text = "";
+        String line;
+        File file = new File(path);
+        try{
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            while ((line = br.readLine()) != null) {
+                if (!line.isEmpty() && !line.equals("usuario, tipo")) {
+                    text += line + "\n";
+                }
+            }
+            if (!"".equals(text)) {
+                String[] nodes = text.split("\n");
+                for (String node : nodes){
+                    String [] nodoInfo = node.split(", ");
+                    User user = new User(nodoInfo[0], nodoInfo[1]);
+                    users.insertAtEnd(user);
+                }
+            }
+            br.close();
+            JOptionPane.showMessageDialog(null, "Archivo cargado exitosamente.");
+            return users;
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Error al leer en archivo de csv. Se procederá sin usuarios registrados.");
+        }
+        return null;
+    }
+    
+    public static void writeCSV(String path, UserList users){
+        if (!users.isEmpty()){
+            try{
+                String text = "usuario, tipo\n";
+                User pAux = users.getpFirst();
+                while (pAux != null) {
+                    text += pAux.getName();
+                    text += ", "+pAux.getType();
+                    text += "\n";
+                    pAux = pAux.getpNext();
+                }
+                PrintWriter pw = new PrintWriter(path);
+                pw.print(text);
+                pw.close();
+                
+            }catch (Exception e){
+                JOptionPane.showMessageDialog(null, "Error al guardar en el archivo de csv. Los cambios no serán guardados."); 
+            }
+        }
+    }
+}
